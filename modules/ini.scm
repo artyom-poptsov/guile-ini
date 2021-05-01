8d824a6207b9bf1303f5f1b18815f9e1183478db
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-05-01 16:50:56 +0300

    modules/ini.scm (scm->ini): New procedure
    * modules/ini.scm (scm->ini): New procedure.
      (%write-section): Likewise.

--- modules/ini.scm
+++ modules/ini.scm
@@ -2,16 +2,17 @@
   #:use-module (ice-9 receive)
   #:use-module (ice-9 pretty-print)
   #:use-module (ice-9 textual-ports)
+  #:use-module (srfi srfi-1)
   #:use-module (oop goops)
   #:use-module (smc fsm)
   #:use-module (ini fsm)
   #:use-module (ini fsm-implementation)
-  #:export (ini->scm))
+  #:export (ini->scm
+            scm->ini))
 
 (define* (ini->scm port #:key (debug-mode? #f))
   (let ((fsm (make <ini-fsm>)))
     (fsm-debug-mode-set! fsm debug-mode?)
-    (pretty-print (hash-table->transition-list (fsm-transition-table fsm)))
     (let loop ((context (make <ini-context>
                           #:module (list (resolve-module '(smc guards char))
                                          (resolve-module '(smc puml))
@@ -22,3 +23,30 @@
             (loop new-context)
             (ini-context-result new-context))))))
 
+
+
+(define (%write-section section port)
+  (let ((title (car section))
+        (props (cdr section)))
+    (when title
+      (format port "[~a]~%" title))
+    (for-each (lambda (prop)
+                (format port "~a=~a~%" (car prop) (cdr prop)))
+              props))
+  (newline port))
+
+(define (scm->ini data port)
+  "Write DATA to a PORT in the INI format. "
+  (let* ((global (find (lambda (section)
+                         (not (car section)))
+                       data))
+         (data   (if global
+                     (delete global data equal?)
+                     data)))
+
+    (when global
+      (%write-section global port))
+
+    (for-each (lambda (section)
+                (%write-section section port))
+              data)))
