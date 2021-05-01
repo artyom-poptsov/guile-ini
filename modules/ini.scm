1509897b36df3f6398df6c78298fd61962c7a709
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-05-01 11:53:15 +0300

    modules/ini.scm (ini->scm): Add 'debug-mode?' named parameter

--- modules/ini.scm
+++ modules/ini.scm
@@ -1,5 +1,6 @@
 (define-module (ini)
   #:use-module (ice-9 receive)
+  #:use-module (ice-9 pretty-print)
   #:use-module (ice-9 textual-ports)
   #:use-module (oop goops)
   #:use-module (smc fsm)
@@ -7,8 +8,9 @@
   #:use-module (ini fsm-implementation)
   #:export (ini->scm))
 
-(define (ini->scm port)
+(define* (ini->scm port #:key (debug-mode? #f))
   (let ((fsm (make <ini-fsm>)))
+    (fsm-debug-mode-set! fsm debug-mode?)
     (let loop ((context (make <ini-context>
                           #:module (list (resolve-module '(smc guards char))
                                          (resolve-module '(smc puml))
