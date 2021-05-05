cd284ce00caf657676420157561f180fb798708d
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-05-05 12:12:45 +0300

    modules/ini (scm->ini): Allow to write comments and newlines
    The overall structure is:
    
    (("section"
      (comment . "Some comment.")
      ("some_property" . "42")
      (newline)
      (comment . "Another comment string.")
      ("another_property" . "value")))
    
    * modules/ini.scm (scm->ini): Allow to write comments and newlines.
      (%default-comment-writer): New procedure.

--- modules/ini.scm
+++ modules/ini.scm
@@ -8,7 +8,8 @@
   #:use-module (ini fsm)
   #:use-module (ini fsm-context)
   #:export (ini->scm
-            scm->ini))
+            scm->ini
+            %default-comment-writer))
 
 (define* (ini->scm port #:key (debug-mode? #f))
   (let ((fsm (make <ini-fsm>)))
@@ -28,17 +29,32 @@
 
 
 
-(define (%write-section section port)
+(define (%default-comment-writer comment port)
+  "This writer adds '; ' before a COMMENT and writes it to a PORT."
+  (format port "; ~a" comment))
+
+(define (%write-section section port comment-writer)
   (let ((title (car section))
         (props (cdr section)))
     (when title
       (format port "[~a]~%" title))
     (for-each (lambda (prop)
-                (format port "~a=~a~%" (car prop) (cdr prop)))
+                (cond
+                 ((string? (car prop))
+                  (format port "~a=~a~%" (car prop) (cdr prop)))
+                 ((equal? (car prop) 'comment)
+                  (comment-writer (cdr prop) port))
+                 ((equal? (car prop) 'newline)
+                  (newline port))
+                 (else
+                  (error "Unknown property type" prop))))
               props))
   (newline port))
 
-(define* (scm->ini data #:key (port (current-output-port)))
+(define* (scm->ini data
+                   #:key
+                   (port           (current-output-port))
+                   (comment-writer %default-comment-writer))
   "Write DATA to a PORT in the INI format. "
   (let* ((global (find (lambda (section)
                          (not (car section)))
@@ -48,8 +64,8 @@
                      data)))
 
     (when global
-      (%write-section global port))
+      (%write-section global port comment-writer))
 
     (for-each (lambda (section)
-                (%write-section section port))
+                (%write-section section port comment-writer))
               data)))
