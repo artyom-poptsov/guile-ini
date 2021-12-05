f9f9252cb26ee01eba005f1ee9dc3e1582721587
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-12-05 09:49:44 +0300

    tests/ini.scm: New test suite
    * tests/ini.scm: New test suite
    * tests/Makefile.am: Update.

---
+++ tests/ini.scm
@@ -0,0 +1,58 @@
+(use-modules (srfi srfi-64)
+             (srfi srfi-26)
+             (oop goops)
+             (ini))
+
+(define %test-name "ini")
+
+
+(test-begin %test-name)
+
+
+;; Taken from Wikipedia:
+;;   <https://en.wikipedia.org/wiki/INI_file>
+(define %test-ini
+  "
+; last modified 1 April 2001 by John Doe
+[owner]
+name=John Doe
+organization=Acme Widgets Inc.
+
+[database]
+; use IP address in case network name resolution is not working
+server=192.0.2.62
+port=143
+file=\"payroll.dat\"
+")
+
+(test-assert "ini->scm: Don't read comments"
+  (with-input-from-string
+      %test-ini
+    (lambda ()
+      (let ((data (ini->scm (current-input-port) #:read-comments? #f)))
+        data))))
+
+(test-assert "ini->scm: Read comments"
+  (with-input-from-string
+      %test-ini
+    (lambda ()
+      (let* ((data   (ini->scm (current-input-port)))
+             (global (cdar data)))
+        (and (equal? (caar global) 'comment)
+             (equal? (cdar global) " last modified 1 April 2001 by John Doe"))))))
+
+(test-assert "scm->ini"
+  (with-input-from-string
+      %test-ini
+    (lambda ()
+      (let ((data   (ini->scm (current-input-port))))
+        (with-output-to-string
+          (lambda ()
+            (scm->ini data)))))))
+
+
+(define exit-status (test-runner-fail-count (test-runner-current)))
+
+(test-end %test-name)
+
+(exit exit-status)
